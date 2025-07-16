e4ecf5df212877d953f473298c2ee26361c0b28c
MTechLab Dev <dliapromua@gmail.com>
2025-07-16 19:36:28 +0300

    publish to my nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,3 +29,16 @@ preprocess {
     vars.put("STANDALONE", 0)
     vars.put("!STANDALONE", 1)
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "nexus"
+            url = uri(project.findProperty("nexusUrl")?.toString() ?: System.getenv("NEXUS_URL"))
+            credentials {
+                username = project.findProperty("nexusUser")?.toString() ?: System.getenv("NEXUS_USERNAME")
+                password = project.findProperty("nexusPass")?.toString() ?: System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
